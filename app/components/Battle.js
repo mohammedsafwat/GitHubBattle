a2288dd8c39bb36f39b25cfcd9387200178d2f65
Mohammed Safwat <mail.msafwat@gmail.com>
2019-01-21 09:35:01 +0100

    Render the player preview component dynamically if the user profile data is available.

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var PropTypes = require('prop-types');
+var Link = require('react-router-dom').Link;
 
 function PlayerPreview (props) {
     return (
@@ -105,6 +106,7 @@ class Battle extends React.Component {
         // We bind the 'this' keyword inside handleSubmit
         // to our instance which is 'this'.
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleReset = this.handleReset.bind(this);
     }
 
     handleSubmit(id, username) {
@@ -116,9 +118,21 @@ class Battle extends React.Component {
         });
     }
 
+    handleReset(id) {
+        this.setState(function () {
+            var newState = {};
+            newState[id + 'Name'] = '';
+            newState[id + 'Image'] = null;
+            return newState;
+        });
+    }
+
     render() {
+        var match = this.props.match;
         var playerOneName = this.state.playerOneName;
         var playerTwoName = this.state.playerTwoName;
+        var playerOneImage = this.state.playerOneImage;
+        var playerTwoImage = this.state.playerTwoImage;
 
         return (
             <div>
@@ -128,14 +142,44 @@ class Battle extends React.Component {
                             id='playerOne' 
                             label='Player One' 
                             onSubmit={this.handleSubmit} 
-                        />}
+                        />
+                    }
+
+                    {playerOneImage !== null &&
+                        <PlayerPreview
+                            avatar={playerOneImage} 
+                            username={playerOneName}
+                            onReset={this.handleReset}
+                            id='playerOne'
+                        />
+                    }
+
                     {!playerTwoName && 
                         <PlayerInput 
                             id='playerTwo' 
                             label='Player Two' 
                             onSubmit={this.handleSubmit} 
-                        />}
+                        />
+                    }
+
+                    {playerTwoImage !== null &&
+                        <PlayerPreview
+                            avatar={playerTwoImage} 
+                            username={playerTwoName}
+                            onReset={this.handleReset}
+                            id='playerTwo'
+                        />
+                    }
                 </div>
+                {playerOneImage && playerTwoImage && 
+                    <Link 
+                        className='button' 
+                        to={{
+                            pathname: match.url + '/results',
+                            search: '?playerOneName=' + playerOneName + '&playerTwoName=' + playerTwoName 
+                        }}>
+                        Battle
+                    </Link>}
             </div>
         )
     }
