9e2bc67629061b4ae9d6d09abfc3cd27f26d37f7
Mohammed Safwat <mail.msafwat@gmail.com>
2019-01-22 23:25:50 +0100

    Add a new method called 'sortPlayers' to sort the players based on who is the winner or the loser.

--- app/utils/api.js
+++ app/utils/api.js
@@ -48,9 +48,17 @@ function getUserData (player) {
     })
 }
 
+function sortPlayers(players) {
+    return players.sort(function (a, b) {
+        return b.score - a.score;
+    });
+}
+
 module.exports = {
     battle: function(players) {
-        
+        return axios.all(players.map(getUserData))
+            .then(sortPlayers)
+            .catch(handleError)
     },
     fetchPopularRepos: function(language) {
         var encodedURI = window.encodeURI(
